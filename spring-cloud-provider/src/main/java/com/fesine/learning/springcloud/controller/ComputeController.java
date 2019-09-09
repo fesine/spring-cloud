cf578af483ccf6d0443843bba6bf06e49278842f
fesine <240944773@qq.com>
2019-09-09 15:59:18 +0800

    修改cloud版本号为1.3.5

---
+++ spring-cloud-provider/src/main/java/com/fesine/learning/springcloud/controller/ComputeController.java
@@ -0,0 +1,37 @@
+package com.fesine.learning.springcloud.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2019/9/9
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2019/9/9
+ */
+@RestController
+public class ComputeController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+}
