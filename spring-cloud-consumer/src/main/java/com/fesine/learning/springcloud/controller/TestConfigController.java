563991b1a2395c211cef874d6c97740a661e0ee4
fesine <240944773@qq.com>
2019-09-12 14:28:53 +0800

    分布式配置中心注册到eureka。

---
+++ spring-cloud-consumer/src/main/java/com/fesine/learning/springcloud/controller/TestConfigController.java
@@ -0,0 +1,27 @@
+package com.fesine.learning.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: 类描述
+ * @author: fesine
+ * @createTime:2019/9/10
+ * @update:修改内容
+ * @author: fesine
+ * @updateTime:2019/9/10
+ */
+@RefreshScope
+@RestController
+public class TestConfigController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return from;
+    }
+}
